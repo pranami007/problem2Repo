8a6378e119bb1d210946dc26d7afc8b3166af4fc
pranami raviraj <praviraj4168@gmail.com>
2020-12-21 13:42:10 +0530

    initialize 2

--- src/Problem2.java
+++ src/Problem2.java
@@ -14,15 +14,14 @@ public class Problem2 {
 	}
 	
 	public static void permutation(String perm, String word) { 
-		if (word.isEmpty()) 
-		{ 
-			System.out.println("Your String is Empty please try again");
-		} 
-		else { 
-			for (int i = 0; i < word.length(); i++) {
+		if (word.isEmpty()) { 
+			System.out.println(perm + word); 
+		}else { 
+			for(int i = 0; i<word.length(); i++) { 
 				permutation(perm + word.charAt(i), word.substring(0, i) + word.substring(i + 1, word.length())); 
-			} 
-		} 
+				} 
+		}
+
 	}
 	
 }
